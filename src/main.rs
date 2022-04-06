024280d22f6d6337b50539f3b11655825c1d2b27
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-04-06 12:11:02 +0300

    create enums to rep language features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+enum Lang {
+    SetVar(String, Value),
+    GetVar(String)
+}
+
+enum Value {
+    int(i64),
+}
+
+fn evaluate(commands: &[Lang]) {}
+
+#[test]
+
+fn test1() {
+    let commands = vec![
+        Lang::SetVar("x".to_string(), Value::int(1)),
+        Lang::GetVar("x".to_string()),
+    ];
+
+    let result = evaluate(&commands);
+}
+
 fn main() {
     println!("Hello, world!");
 }
